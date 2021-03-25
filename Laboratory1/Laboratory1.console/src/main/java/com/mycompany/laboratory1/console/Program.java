4a3a6a11d4a84749353c5c7ac65e01d4d41418fd
Maxim Zimin <zim.ma2010@yandex.ru>
2021-03-25 07:56:20 +0300

    change contract

--- Laboratory1/Laboratory1.console/src/main/java/com/mycompany/laboratory1/console/Program.java
+++ Laboratory1/Laboratory1.console/src/main/java/com/mycompany/laboratory1/console/Program.java
@@ -5,10 +5,11 @@
  */
 package com.mycompany.laboratory1.console;
 
-import com.mycompany.laboratory1.client.FieldFind;
-import com.mycompany.laboratory1.client.FindStudentResponse;
-import com.mycompany.laboratory1.client.FindStudentsRequest;
-import com.mycompany.laboratory1.client.StudentWebService_Service;
+import com.mycompany.laboratory1.console.client.FieldFind;
+import com.mycompany.laboratory1.console.client.FindStudentResponse;
+import com.mycompany.laboratory1.console.client.FindStudentsRequest;
+import com.mycompany.laboratory1.console.client.StudentWebService_Service;
+import com.mycompany.laboratory1.console.factory.ServiceEntityFactory;
 import java.util.List;
 
 /**
@@ -16,13 +17,15 @@ import java.util.List;
  * @author zimma
  */
 public class Program {
-    public static void main(String[] args){
-        
-        StudentWebService_Service studentWebService_Service = new StudentWebService_Service( );
-        
+
+    public static void main(String[] args) {
+
+        StudentWebService_Service studentWebService_Service = new StudentWebService_Service();
+
         FindStudentsRequest findStudentsRequest = new FindStudentsRequest();
         List<FieldFind> fieldFinds = findStudentsRequest.getFieldFinds();
-        
+        fieldFinds.add(ServiceEntityFactory.createFieldFind("id", 1));
+
         FindStudentResponse findStudents = studentWebService_Service.getStudentWebServicePort().findStudents(findStudentsRequest);
     }
 }
